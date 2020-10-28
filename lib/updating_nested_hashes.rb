affaf6bc8308f4a11e00e43e7d9c921bd44152d9
Erwin Feng <efeng732@gmail.com>
2020-10-28 21:55:27 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -53,7 +53,7 @@ def add_characters
        
          
        },
-       :additional_characters => []
+       :additional_characters => [{:name => "Prince Escalus"}, {:name => "Apothecary"} ]
       }
    }
 
